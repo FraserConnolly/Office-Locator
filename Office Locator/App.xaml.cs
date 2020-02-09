94844f1705695b8180c173f3da1914c2372bcd30
FraserConnolly <35930036+FraserConnolly@users.noreply.github.com>
2020-02-09 12:37:29 +0000

    Project created.

--- Office Locator/App.xaml.cs
+++ Office Locator/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Office_Locator
+namespace OffstageControls.OfficeLocator
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Office Locator/Properties/AssemblyInfo.cs
+++ Office Locator/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle( "Office Locator" )]
-[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyDescription( "By Fraser Connolly" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyCompany( "Offstage Controls" )]
 [assembly: AssemblyProduct( "Office Locator" )]
 [assembly: AssemblyCopyright( "Copyright ©  2020" )]
 [assembly: AssemblyTrademark( "" )]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "1.0.2.20040" )]
+[assembly: AssemblyFileVersion( "1.0.2.20040" )]
